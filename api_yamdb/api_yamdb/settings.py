20b5c4411403f598c19e3bd1bf27af0d30e0544e
Никита Шелепов <shelepovfamily@gmail.com>
2023-04-25 15:59:56 +0500

    fixed settings.py INSTALLED APPS

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -15,7 +15,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'rewiews.apps.RewiewsConfig'
+    'rewiews.apps.RewiewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
