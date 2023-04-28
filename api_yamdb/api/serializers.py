135376d2f9a94808c6c7ad6763f4ec7bc657038b
Di <120574516+terrazavr@users.noreply.github.com>
2023-04-28 10:18:30 +0900

    new app api

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,72 @@
+from rest_framework import serializers
+from reviews.models import (
+    Category,
+    Genre,
+    Title,
+    Review,
+    Comment
+)
+
+
+class CategotySerializer(serializers.ModelSerializer):
+    """Сериализатор для категорий."""
+
+    class Meta:
+        fields = ('name', 'slug')
+        model = Category
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    """Сериализатор жанров."""
+
+    class Meta:
+        fields = ('name', 'slug')
+        model = Genre
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    """Сериализатор произведений."""
+    genre = serializers.SlugRelatedField(
+        queryset=Genre.objects.all(),
+        read_only=True,
+        slug_field='slug'
+    )
+    category = serializers.SlugRelatedField(
+        queryset=Category.objects.all(),
+        read_only=True,
+        slug_field='slug'
+    )
+
+    class Meta:
+        fields = ('id', 'name', 'year', 'description', 'genre', 'category')
+        model = Title
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    """Сериализатор отзывов"""
+    author = serializers.SlugRelatedField(
+        default=serializers.CurrentUserDefault(),
+        read_only=True,
+        slug_field='username'
+    )
+
+    class Meta:
+        fields = ('id', 'title', 'text', 'author', 'score', 'pub_date')
+        model = Review
+
+
+class CommentReadSerializer(serializers.ModelSerializer):
+    """Сериализатор просмотра отзывов."""
+    author = serializers.CharField(source="author.username", read_only=True)
+
+    class Meta:
+        fields = ('id', 'review', 'text', 'author', 'pub_date')
+        model = Comment
+
+
+class CommentWriteSerializer(serializers.ModelSerializer):
+    author = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        fields = ('id', 'review', 'text', 'author', 'pub_date')
+        model = Comment
