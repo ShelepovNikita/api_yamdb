588b8a35661737ab9443e47740b423d006538eae
Di <120574516+terrazavr@users.noreply.github.com>
2023-04-30 23:57:13 +0900

    views with permissions and filters

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
 
-from rest_framework import viewsets, permissions, filters
+from rest_framework import viewsets, filters
+
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from reviews.models import (
     Category,
@@ -17,45 +20,74 @@ from .serializers import (
     ReviewSerializer,
     CommentReadSerializer
 )
+from .filters import TitleFilter
+from .permissions import (
+    IsAdminOrReadOnly,
+    IsAuthorOrModerOrAdminOrReadOnly)
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
+    """API для модели категорий."""
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     pagination_classes = LimitOffsetPagination
-    # дописать пермишен кому доступны категории
-    # дописать filter_backends и serch_fields
+    filter_backends = (filters.SearchFilter)
+    search_fields = ('name', )
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class GenreViewSet(viewsets.ModelViewSet):
+    """API для модели жанров."""
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     pagination_classes = LimitOffsetPagination
-    # дописать пермишен кому доступны категории
-    # дописать filter_backends и serch_fields
+    filter_backends = (filters.SearchFilter)
+    search_fields = ('name', )
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class TitleViewsSet(viewsets.ModelViewSet):
+    """API для модели произведений."""
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
     pagination_classes = LimitOffsetPagination
-    # дописать пермишен кому доступны категории
-    # фильтрация по категории и жанру
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TitleFilter
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
+    """API для модели отзывов."""
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     pagination_classes = LimitOffsetPagination
-    # дописать пермишен кому доступны категории
-    # тут будет perform create
-    # а тут get_queryset
+    permission_classes = (IsAuthenticatedOrReadOnly,
+                          IsAuthorOrModerOrAdminOrReadOnly,)
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        serializer.save(author=self.request.user, title_id=title_id)
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        review_queryset = Review.objects.filter(title=title_id)
+        return review_queryset
 
 
 class CommentReadViewSet(viewsets.ModelViewSet):
+    """API для модели комментариев."""
     queryset = Comment.objects.all()
     serializer_class = CommentReadSerializer
     pagination_classes = LimitOffsetPagination
-    # дописать пермишен кому доступны категории
-    # тут будет perform create
-    # а тут get_queryset
+    permission_classes = (IsAuthenticatedOrReadOnly,
+                          IsAuthorOrModerOrAdminOrReadOnly,)
+
+    def get_queryset(self):
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=review_id)
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=review_id)
+        serializer.save(author=self.request.user, review=review)
