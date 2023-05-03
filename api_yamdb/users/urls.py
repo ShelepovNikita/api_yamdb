2cdd5971381f2c433ab489828b1c6be4c7ee4629
Никита Шелепов <shelepovfamily@gmail.com>
2023-05-03 14:05:50 +0500

    Created routers for users app

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -1,14 +1,18 @@
+from rest_framework.routers import SimpleRouter
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
 
-from .views import UsersViewSet, get_token, sign_up
+from users.views import (
+    UsersViewSet,
+    get_token,
+    sign_up
+)
+from api_yamdb.settings import VERSION_API
 
 
-router_v1 = DefaultRouter()
+router_v1 = SimpleRouter()
 router_v1.register(r'users', UsersViewSet)
-
 urlpatterns = [
-    path('v1/', include(router_v1.urls)),
-    path('v1/auth/signup/', sign_up, name='sign_up'),
-    path('v1/auth/token/', get_token, name='get_token'),
+    path(f'{VERSION_API}', include(router_v1.urls)),
+    path(f'{VERSION_API}auth/signup/', sign_up, name='sign_up'),
+    path(f'{VERSION_API}auth/token/', get_token, name='get_token'),
 ]
