6e44012b6a5260d34ca72c5fab38f5fa47bfa01f
Никита Шелепов <shelepovfamily@gmail.com>
2023-05-03 13:58:06 +0500

    Delete users model from app

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 
-from .models import (
-    User,
+from reviews.models import (
     Category,
     Genre,
     Title,
     Review,
     Comment)
 
-admin.site.register(User)
 admin.site.register(Category)
 admin.site.register(Genre)
 admin.site.register(Title)
