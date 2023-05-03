aaf79eaeac822196919ff9b29ab8ab3f3eaa9e5b
Никита Шелепов <shelepovfamily@gmail.com>
2023-05-03 14:07:13 +0500

    Created viewsets.py

---
+++ api_yamdb/api/viewsets.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+from rest_framework import mixins
+
+
+class CreateListDestroyViewSet(mixins.CreateModelMixin,
+                               mixins.ListModelMixin,
+                               mixins.DestroyModelMixin,
+                               viewsets.GenericViewSet):
+    pass
