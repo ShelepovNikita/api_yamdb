afe3e4199eb568b2571e2d415e031aff5b9b5980
Никита Шелепов <shelepovfamily@gmail.com>
2023-05-03 13:56:13 +0500

    Fixed imports

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
-from .models import User
+from users.models import User
 
 
 class SignUpSerializer(serializers.ModelSerializer):
